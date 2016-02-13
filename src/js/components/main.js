9185f2fff194dc56430cb325ed2aff488699af59
naoishii <drapuni@gmail.com>
2016-02-13 16:32:55 +0900

    ステージ

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -9,19 +9,15 @@ export default function main(engine) {
   const Events = Matter.Events;
 
   // create two boxes and a ground
-  const circleA = Bodies.circle(200, 200, 40, {
+  const circleA = Bodies.circle(300, 0, 20, {
     density: 0.001, // 質量
     frictionAir: 0.01, // 空気抵抗
     restitution: 1, // 弾力性
     friction: 0.01 // 摩擦
   });
 
-  const boxA = Bodies.rectangle(40, 50, 80, 80);
-  const boxB = Bodies.rectangle(150, 50, 80, 80);
-
   // add all of the bodies to the world
-  World.add(engine.world, [circleA, boxA, boxB, ...stage()]);
-
+  World.add(engine.world, [circleA, ...stage()]);
 
   Events.on(engine, 'collisionStart', () => {
     console.log(circleA.position.x, circleA.position.y);
--- src/js/components/stage.js
+++ src/js/components/stage.js
@@ -9,8 +9,17 @@ export default function getWorld() {
   const wallR = Bodies.rectangle(STAGE_WIDTH, STAGE_HEIGHT / 2, 10, STAGE_HEIGHT, { isStatic: true });
   const wallL = Bodies.rectangle(0, STAGE_HEIGHT / 2, 10, STAGE_HEIGHT, { isStatic: true });
 
+  const bar1 = Bodies.rectangle(300, 80, 300, 10, {
+    isStatic: true,
+    angle: Math.PI * -0.06
+  });
+  const bar2 = Bodies.rectangle(100, 200, 300, 10, {
+    isStatic: true,
+    angle: Math.PI * -0.06
+  });
+
 
   return [
-    ground, wallR, wallL
+    ground, wallR, wallL, bar1, bar2
   ];
 }
