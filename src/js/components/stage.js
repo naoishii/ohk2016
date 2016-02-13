b0bc023b543286b64f8e419f02576d91885b5866
naoishii <drapuni@gmail.com>
2016-02-14 01:35:02 +0900

    いちめん

--- src/js/components/stage.js
+++ src/js/components/stage.js
@@ -9,18 +9,35 @@ export default function getWorld() {
   const wallR = Bodies.rectangle(STAGE_WIDTH, STAGE_HEIGHT / 2, 10, STAGE_HEIGHT, { isStatic: true });
   const wallL = Bodies.rectangle(0, STAGE_HEIGHT / 2, 10, STAGE_HEIGHT, { isStatic: true });
 
-  const bar1 = Bodies.rectangle(300, 80, 300, 10, {
+  const bar1 = Bodies.rectangle(300, 80, 400, 10, {
     isStatic: true,
     angle: Math.PI * -0.06
   });
-  const bar2 = Bodies.rectangle(100, 200, 300, 10, {
+  const bar2 = Bodies.rectangle(90, 200, 100, 10, {
     isStatic: true,
     angle: Math.PI * -0.06
+  });
+  const bar3 = Bodies.rectangle(260, 170, 100, 10, {
+    isStatic: true,
+    angle: Math.PI * -0.06
+  });
+  const bar4 = Bodies.rectangle(330, 170, 70, 10, {
+    isStatic: true,
+    angle: Math.PI * 0.06
+  });
+  const bar5 = Bodies.rectangle(190, 230, 200, 10, {
+    isStatic: true,
+    angle: Math.PI * 0.06
   });
 
+  const bar6 = Bodies.rectangle(290, 320, 200, 10, {
+    isStatic: true,
+    angle: Math.PI * 0.06
+  });
 
   return [
-    ground, wallR, wallL, bar1, bar2
+    ground, wallR, wallL, bar1, bar2, bar3, bar4, bar5,
+    bar6, 
   ];
   
 }
